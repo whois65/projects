ca60413cba66557ca37cc3d05b7b3128756aa784
Raffiqi <raffi652009@gmail.com>
2025-12-23 20:51:59 +0700

    Delete forIca directory

--- forIca/function.js
+++
@@ -1,8 +0,0 @@
-const qi = (x) => document.getElementById(x);
-const qs = (x) => document.querySelector(x);
-const delay = (ms) => new Promise(res => setTimeout(res, ms));
-const randomInt = (max) => Math.floor(Math.random() * max);
-const randomNum = (max) => {
-    const n = Math.floor(Math.random() * (max + 1));
-    return n % 2 === 0 ? -n : n;
-};
